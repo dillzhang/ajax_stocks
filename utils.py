7e789f4f6f9298c301172ae6f94fea8b8e3cf299
lorenrose1013 <lorenrose1013@aol.com>
2015-12-10 22:23:59 -0500

    api stuff

--- utils.py
+++ utils.py
@@ -6,11 +6,30 @@ import urllib2, json
 def getJSON(stockTicker):
     url = "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol="
     url += stockTicker
-    url += "&callback=myFunction"
     
     page = urllib2.urlopen(url)
     responce = json.load(page)
     print responce.keys()
-    print responce['Name']
+    print responce['LastPrice']
+    print responce['ChangeYTD']
     return responce
 getJSON("AAPL")
+
+def estimateReturn(stockTicker, investment):
+	stockData = getJSON(stockTicker)
+	origPrice = stockData['ChangeYTD']
+	lastPrice = stockData['LastPrice']
+	numStocks = investment * 1.0 / origPrice 
+	currWorth = numStocks * lastPrice
+	netProfit = currWorth - investment
+	return netProfit
+print estimateReturn("AAPL", 1000000)
+
+def guessTicker(company):
+	url = "http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input="
+	url += company
+	page = urllib2.urlopen(url)
+	responce = json.load(page)
+	if (len(responce) < 1):
+		return "Error, Stock Ticker not found"
+	return responce[0]['Symbol']
